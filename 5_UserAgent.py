6ab1bca641cbfb18d877458f8234637c66904321
unli <unlimit@gmail.com>
2022-04-06 21:33:35 +0900

    requests 연습

--- 5_UserAgent.py
+++ 5_UserAgent.py
@@ -1,8 +1,10 @@
-# https://www.whatismybrowser.com/detect/what-is-my-user-agent/
-
 import requests
 url = "http://nadocoding.tistory.com"
-headers = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36"
-res = requests.get(url, headers=headers)
+
+# https://www.whatismybrowser.com/detect/what-is-my-user-agent/
+# header = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36"
+header={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36"}
+res = requests.get(url, headers=header) # 왜 user agent 를 넣으면 에러가 날까요...??? header 의 형식에 주의
+res.raise_for_status()
 with open("nado.html", "w", encoding="utf8") as f:
     f.write(res.text)
